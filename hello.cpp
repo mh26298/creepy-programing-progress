92d6688340bfff065ae2cfd6dfc8a265be338f17
mh26298 <mh26298@my.open.ac.uk>
2015-10-22 10:10:04 +0100

    1.st Hello

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+	std::cout<<"Hello\n";
+	std::cout<<"Number 11: " <<11<<"\n";
+	std::cout<<"Formating object std::endl ";
+	std::cout<<"causes brake line on screen.";
+	std::cout<<std::endl;
+	std::cout<<"Some big number:\t\t" << 70000; /* /t represents TAB */
+	std::cout<<std::endl;
+	std::cout<<"Some Math examples:\t";
+	std::cout<<"Sum of 9 + 6:\t";
+	std::cout<<9+6<<std::endl;
+	std::cout<<"Dividing 10/3:\t\t";
+	std::cout<<(float)10/3<< std::endl;
+	std::cout<<"Multipling big number:\t";
+	std::cout<<(double)9666*6999<< std::endl;
+	return 0;
+}
